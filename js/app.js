4d863f9fe52a299bc47a444b5da5b08272732eb1
fullyslick <default@mail.com>
2018-03-12 15:34:42 +0200

    Fix pop up dialog errors

--- js/app.js
+++ js/app.js
@@ -39,62 +39,54 @@ function shuffle(array) {
  * and replaces the "deck" of cards with new one.
  * TO:DO - reset timer and reset stars
  */
- function newGame(){
-   // Make all cards closed and unmatched
-   for (let i = 0; i < allCards.length; i++) {
-     allCards[i].classList.remove("match", "show", "open") ;
-   }
-   // Call the helper function that shuffles the cards
-   shuffle(allCards);
+function newGame() {
+  // Game is restared so it is not over
+  isGameOver = false;
 
-   // Fragment to hold the new "deck" of cards
-   let newCardFragment = document.createDocumentFragment();
+  // Make all cards closed and unmatched
+  for (let i = 0; i < allCards.length; i++) {
+    allCards[i].classList.remove("match", "show", "open");
+  }
+  // Call the helper function that shuffles the cards
+  shuffle(allCards);
 
-   // Remove the currently displayed cards
-   for (var i = 0; i < allCards.length; i++) {
-     deck.removeChild(allCards[i]);
-   }
+  // Fragment to hold the new "deck" of cards
+  let newCardFragment = document.createDocumentFragment();
 
-   // Loop through each card and create its HTML
-   for (let i = 0; i < shuffleOutput.length; i++) {
-     newCardFragment.appendChild(shuffleOutput[i]);
-   }
+  // Remove the currently displayed cards
+  for (var i = 0; i < allCards.length; i++) {
+    deck.removeChild(allCards[i]);
+  }
 
-   // Add each card's HTML to the page
-   deck.appendChild(newCardFragment);
- }
+  // Loop through each card and create its HTML
+  for (let i = 0; i < shuffleOutput.length; i++) {
+    newCardFragment.appendChild(shuffleOutput[i]);
+  }
 
-// When the page is loaded change the position of teh cards
-window.onload = function(){
+  // Add each card's HTML to the page
+  deck.appendChild(newCardFragment);
+}
+
+// When the page is loaded change the position of the cards
+window.onload = function() {
   newGame();
 }
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */console.log("All cards are " + allCards[i]);
-
-/*
  * @description Displays pop up dialog
  */
-function showPopUpDialog(){
+function showPopUpDialog() {
   // Display popUpDialog
   popUpDialog.style.display = "block";
 
   // If game is over display congratulations message and mettrics
   if (isGameOver) {
-    // TO:DO Display metrics
+    // TO:DO Display metrics time and stars
 
     // Display congratulations message
     popUpMessage.textContent = "Congratulations! You won the game!"
     // Change the button message
-    playAgainBtn.textContent = "Play Again"
+    playAgainBtn.textContent = "Play Again!"
     // Hide close dialog button
     closeDialogBtn.style.display = "none";
   } else {
@@ -106,7 +98,7 @@ function showPopUpDialog(){
   }
 
   // Reset game on "play-again"/"restart" button click and close dialog
-  playAgainBtn.addEventListener("click", function(){
+  playAgainBtn.addEventListener("click", function() {
     newGame();
 
     hidePopUpDialog();
@@ -116,7 +108,7 @@ function showPopUpDialog(){
 /*
  * @description Hides pop up dialog
  */
-function hidePopUpDialog(){
+function hidePopUpDialog() {
   popUpDialog.style.display = "none";
 }
 
@@ -124,8 +116,10 @@ function hidePopUpDialog(){
  * Event listener to reset the game on "restart" button click
  * @description Calls pop up dialog appears to confirm the restart,
  */
-restartBtn.addEventListener("click" , function(){
+restartBtn.addEventListener("click", function() {
   // TO:DO Should Pause The Game Timer
+  // Display close button if it is cleared by previous restart of the game
+  closeDialogBtn.style.display = "block";
 
   showPopUpDialog();
 });
@@ -133,10 +127,21 @@ restartBtn.addEventListener("click" , function(){
 /*
  * Close pop up dialog
  */
-closeDialogBtn.addEventListener("click", function(){
+closeDialogBtn.addEventListener("click", function() {
   // TO:DO Unpause timer
   hidePopUpDialog();
 });
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
 
 // The code below will display all cards. Use to check if shuffeling the cards is working
 function displayAllCards() {
